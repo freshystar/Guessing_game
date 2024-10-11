f3b4f99280a53bb02d298cb94a902317b7c7845a
GIS Student <gis@MacBook-Pro-von-GIS.local>
2024-10-11 10:51:56 +0100

    guessing number game is working for now

--- guess_number.sh
+++ guess_number.sh
@@ -1,13 +1,14 @@
 guessing_game() {
     rand_number=$((RANDOM % $1))
+    guess_numbers=($2 $3 $4)
     for i in {1..3}; do
         echo "Guess the number:" #prompt the user to enter a value
-        read guess_number #read input from the user
-
-        if [ $rand_number -eq $guess_number ]; then
-            echo "Congratulations, You Won!"
+        #read guess_number       #read input from the user
+        guess_number=${guess_numbers[$i]}
+        if [ "$rand_number" -eq "$guess_number" ]; then
+            echo "Congratulations, You Won!, the number is $rand_number "
             return
-        elif [ $rand_number -gt $guess_number ]; then
+        elif [ "$rand_number" -gt "$guess_number" ]; then
              echo "You missed! The random number is greater than $guess_number"
         else
             echo "You missed! The random number is lesser than $guess_number"
